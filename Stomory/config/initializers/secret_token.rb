cc1cdc2cf8bf99750ba42838a2b2a85998ebb637
Dang Nguyen <dang.nguyen@nemesu.com>
2012-05-27 04:04:42 -0400

    first commit

---
+++ Stomory/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stomory::Application.config.secret_token = '22528dbc0bb0d6d7b23c0926c634eba1a11116b811186d29732c8f0dd423d045535e6db697177ecfeedda089d42d1d9712c2b7d74d124c5350b29480850a03a4'
